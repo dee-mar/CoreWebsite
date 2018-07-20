80d54a8266ee6ea75f3a1ccfda034726b6d0faa9
David <David@DESKTOP-G0QTME3>
2018-07-20 17:07:28 -0500

    First commit

---
+++ CoreWebsite/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CoreWebsite.Models;
+using CoreWebsite.Models.SteamApi;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
+
+namespace CoreWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IConfiguration _configuration;
+
+        public HomeController(IConfiguration Configuration)
+        {
+            _configuration = Configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public IActionResult About()
+        //{
+        //    ViewData["Message"] = "Your application description page.";
+
+        //    return View();
+        //}
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<ActionResult> About()
+        {
+            GetOwnedGamesResponse oResponse = new GetOwnedGamesResponse();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://api.steampowered.com/");
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
+                string sAPIKey = _configuration["Steam:ApiKey"];
+                string sSteamId = _configuration["Steam:SteamId"];
+                HttpResponseMessage Res = await client.GetAsync("IPlayerService/GetOwnedGames/v0001/?key=" + sAPIKey + "&steamid=" + sSteamId + "&format=json&include_appinfo=1");
+
+                //Test Call https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=573995896871E7891B78DA9E841FCEB5&steamid=76561197960434622&format=json&include_appinfo=1
+
+                //Checking the response is successful or not which is sent using HttpClient  
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api   
+                    var SteamResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing into the Employee list  
+                    oResponse = JsonConvert.DeserializeObject<GetOwnedGamesResponse>(SteamResponse);
+
+                    Console.WriteLine(oResponse.Response.game_count);
+                }
+
+                return View(oResponse.Response);
+            }
+        }
+    }
+}
---
+++ CoreWebsite/Models/SteamUserModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoreWebsite.Models.SteamApi
+{
+    public class GetOwnedGamesResponse
+    {
+        private OwnedGamesSummary oOwndedGames;
+        public OwnedGamesSummary Response { get => oOwndedGames; set => oOwndedGames = value; }
+    }
+
+    public class OwnedGamesSummary
+    {
+        private int iGame_count;
+        private List<OwnedGameDetail> oGames;
+
+        public int game_count { get => iGame_count; set => iGame_count = value; }
+        public List<OwnedGameDetail> games { get => oGames; set => oGames = value; }
+    }
+
+    public class OwnedGameDetail
+    {
+        private int iAppid;
+        private string sName;
+        private long lPlaytime_2weeks;
+        private long lPlaytime_forever;
+        private string sImg_icon_url;
+        private string sImg_logo_url;
+        private bool bHas_community_visible_stats;
+
+        public int appid { get => iAppid; set => iAppid = value; }
+        public string name { get => sName; set => sName = value; }
+        public long playtime_2weeks { get => lPlaytime_2weeks; set => lPlaytime_2weeks = value; }
+        public long playtime_forever { get => lPlaytime_forever; set => lPlaytime_forever = value; }
+        public string img_icon_url { get => "http://media.steampowered.com/steamcommunity/public/images/apps/" + this.appid + "/"+ sImg_icon_url + ".jpg"; set => sImg_icon_url = value; }
+        public string img_logo_url { get => "http://media.steampowered.com/steamcommunity/public/images/apps/" + this.appid + "/" + sImg_logo_url + ".jpg"; set => sImg_logo_url = value; }
+        public bool has_community_visible_stats { get => bHas_community_visible_stats; set => bHas_community_visible_stats = value; }
+    }
+
+    public class GetSteamPlayerSummariesResponse
+    {
+        private SteamPlayerSummaries oResponse;
+        public SteamPlayerSummaries Response { get => oResponse; set => oResponse = value; }
+    }
+    
+    public class SteamPlayerSummaries
+    {
+        private List<SteamPlayerSummary>  oPlayers;
+        public List<SteamPlayerSummary> Players { get => oPlayers; set => oPlayers = value; }
+    }
+
+    public class SteamPlayerSummary
+    {
+        private int steamid { get; set; }
+        private int communityvisibilitystate { get; set; }
+        public int profilestate { get; set; }
+        public string personaname { get; set; }
+        public int lastlogoff { get; set; }
+        public string profileurl { get; set; }
+        public string avatar { get; set; }
+        public string avatarmedium { get; set; }
+        public string avatarfull { get; set; }
+        public int personastate { get; set; }
+        public string realname { get; set; }
+        public int primaryclanid { get; set; }
+        public double timecreated { get; set; }
+        public int personastateflags { get; set; }
+        public string loccountrycode { get; set; }
+        public string locstatecode { get; set; }
+        public int loccityid { get; set; }
+    }
+}
